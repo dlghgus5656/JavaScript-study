7c7d591f6224cc51e3651d33f193360946f14540
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-01-24 23:24:11 +0900

    clock
    시계 기능 구현(Date, setInterval, setTimeout, padStart, padEnd)

--- study/app.js
+++ study/app.js
@@ -218,3 +218,23 @@ function handleTitleClick() {
 }
 
 h1.addEventListener("click", handleTitleClick);
+
+"1".padStart(2, "0"); // 1이란 숫자를 출력하는데 길이는 2여야 하고 그렇지 않은 경우엔 앞에 0을 넣어준다 = 01
+"1".padEnd(2, "0"); // 1이란 숫자를 출력하는데 길이는 2여야 하고 그렇지 않은 경우엔 앞에 0을 넣어준다 = 10
+//만약 길이를 2로 주지않고 3으로 주었을 경우엔 앞이나 뒤에 00이 붙는다.
+
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0"); // padStart: 길이를 2로 지정한다 길이가 2보다 작을경우 앞에서부터 0으로 채운다.(01)
+  const minutes = String(date.getMinutes()).padStart(2, "0"); //padEnd: 길이를 2로 지정한다 길이가 2가 아닐경우 뒤에서부터 0으로 채운다.(10)
+  const second = String(date.getSeconds()).padStart(2, "0"); // 참고로 padStart,End 는 string 값으로 받아야 사용가능하다.(String으로 묶어주는 이유)
+
+  clock.innerText = `${hours}:${minutes}:${second}`;
+}
+
+getClock();
+setInterval(getClock, 1000); //5초마다 실행
+
+//setTimeout(sayHello, 5000); // 5초 기다렸다가 한번만 실행
